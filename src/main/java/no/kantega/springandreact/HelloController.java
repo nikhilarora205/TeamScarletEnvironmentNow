f69fe217500654246a0fc659c033ccdd265d1ca8
Nikhil Arora <narora205@gmail.com>
2019-10-12 21:00:39 -0500

    controller About

--- src/main/java/no/kantega/springandreact/HelloController.java
+++ src/main/java/no/kantega/springandreact/HelloController.java
@@ -4,11 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @RestController
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        //return "Hello, the time at the server is now " + new Date() + "\n";
+        String myUrl = "https://api.github.com/repos/nikhilarora205/TeamScarletEnvironmentNow/stats/commit_activity";
+        //String to place our result in
+        String result;
+        HttpGetRequest getRequest = new HttpGetRequest();
+        result = getRequest.execute(myUrl).get();
+        JSONObject myResponse = new JSONObject(result.toString());
+        String total = myResponse.getJSONObject("total").toString();
+        return total;
     }
 }
