03b6dfb0b8dcc7ff049cccbd813dd1453aca87d8
ss78832 <s98samman@utexas.edu>
2019-10-23 16:41:26 -0500

    Old strategy code cleaned up/deleted

--- Main.java
+++ Main.java
@@ -99,39 +99,7 @@ public class Main {
 		
 	}
 	
-	public void submittingForm() throws Exception {
-		/*
-	        // Get the first page
-	        final HtmlPage page1 = webClient.getPage("https://www.adt.com/natural-disasters");
-	        //wait some time???
-	        //??????
-
-	        // Get the form that we are dealing with
-	        // The form on the website does not have a button
-	        //2 options: 
-	        //1. simulate "enter" keypress within text input box
-	        //2. create button and append it to the form
-	        DomElement e = page1.getElementById("pac-input");							//2 separate trials
-	        DomElement eHtml = page1.getHtmlElementById("pac-input");					//(both should work)
-	        final HtmlForm form1 = (HtmlForm) e;
-	        final HtmlForm form2 = (HtmlForm) eHtml;
-	       
-	        HtmlElement button = (HtmlElement) page1.createElement("button");			//create button to submit
-	        button.setAttribute("type", "submit");
-	        
-	        form1.appendChild(button);
-
-	        final HtmlSubmitInput button1 = (HtmlSubmitInput) button;
-	        //final HtmlTextInput textField = form.getInputByName("userid");
-
-	        // Change the value of the text field
-	        //textField.type("houston Texas");
-
-	        // Now submit the form by clicking the button and get back the second page.
-	        //final HtmlPage page2 = button.click();
-		*/  
-		    
-		    
+	public void submittingForm() throws Exception {		    
 		//System.setProperty("webdriver.gecko.driver","C:\\Users\\Sammy\\eclipse-workspace\\textInputSubmitJava\\drivers\\geckodriver.exe");
 		//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		//capabilities.setCapability("marionette",true);
