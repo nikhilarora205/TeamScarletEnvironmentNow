8d1bd17d1b3e93ac45bcb82bb491ad778b39e740
ss78832 <s98samman@utexas.edu>
2019-10-24 17:37:26 -0500

    Uses headless chromedriver

--- src/main/java/no/kantega/springandreact/HelloController.java
+++ src/main/java/no/kantega/springandreact/HelloController.java
@@ -27,6 +27,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 import com.gargoylesoftware.htmlunit.*;
@@ -240,7 +241,9 @@ public class HelloController {
           
           String projDir = System.getProperty("user.dir");
   		System.setProperty("webdriver.chrome.driver", projDir + "//drivers//chromedriver");
-  		WebDriver driver = new ChromeDriver();
+  		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+		WebDriver driver = new ChromeDriver(options);
   		
   		JSONObject json = new JSONObject();
   			
