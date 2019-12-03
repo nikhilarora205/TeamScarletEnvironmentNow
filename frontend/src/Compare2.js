0f80a1a7222df492006cab3d2afbb446bcddfb47
KhalidAhmad95 <khalid.ashraf.ahmad@gmail.com>
2019-12-02 22:52:32 -0600

    Compare2 now updated to reflect back end updates

--- frontend/src/Compare2.js
+++ frontend/src/Compare2.js
@@ -29,7 +29,6 @@ const backdropStyle = {
 	backgroundColor: 'darkGray'
 }
 
-
 const headerStyle = {
     color: 'Black',
     backgroundColor: 'lightGray',
@@ -46,21 +45,9 @@ class Compare2 extends Component{
 	state = {
 			isLoading: true,
 			address: "",
-			air: [],
-			allergen: [],
-			water: [],
-			natural: [],
 			address2: "",
-			air2: [],
-			allergen2: [],
-			water2: [],
-			natural2: [],
-			bar1: [],
-			bar2: [],
-			bar3: [],
-			bar1_: [],
-			bar2_: [],
-			bar3_: []
+			masterData: [],
+			masterData2: []
 		  };
 
 	async componentDidMount() {
@@ -76,105 +63,13 @@ class Compare2 extends Component{
 			this.address = addresses[0];
 			this.address2 = addresses[1];
 			
+			const masterResponse = await fetch('/api/getData/'+this.address)
+			const masterResponseBody = await masterResponse.json();
+			this.setState({masterData: masterResponseBody,isLoading: true });
 			
-			//
-			// have to separate both addresses
-			// this.address = "100 Orvieto Cove, Liberty Hill, TX, 78642";
-			// this.address2 = "15504 Cinca Terra Drive, Austin, TX, 78738";
-			//
-
-			// debug statements
-			// console.log(this.address);
-			// console.log(this.address2);
-		
-			const AQIResponse = await fetch('/api/AQIData/'+this.address);
-			// const AQIResponse = await fetch('/api/AQIData');
-			const AQIBody = await AQIResponse.json();
-			this.setState({ air: AQIBody, isLoading: true });
-			// console.log(this.state.air["airData"]);
-
-			const waterResponse = await fetch('/api/waterData/'+this.address);
-			// const waterResponse = await fetch('/api/waterData');
-		    const waterBody = await waterResponse.json();
-			this.setState({ water: waterBody, isLoading: true });
-			// console.log(this.state.water["waterData"]);
-
-			const natResponse = await fetch('/api/naturalDisasters/'+this.address);
-			// const natResponse = await fetch('/api/naturalDisasters');
-		    const natBody = await natResponse.json();
-			this.setState({ natural: natBody, isLoading: true });
-			// console.log(this.state.natural["natData"]);
-			
-			const allergenResponse = await fetch('/api/allergenData/' + this.address);
-			const allergenBody = await allergenResponse.json();
-			this.setState({ allergen: allergenBody, isLoading:true});
-			// console.log(this.state.allergen);
-			
-
-			// // second address data
-			const AQIResponse2 = await fetch('/api/AQIData/'+this.address2);
-			// const AQIResponse = await fetch('/api/AQIData');
-			const AQIBody2 = await AQIResponse2.json();
-			this.setState({ air2: AQIBody2, isLoading: true });
-			// console.log(this.state.air2["airData"]);
-
-			const waterResponse2 = await fetch('/api/waterData/'+this.address2);
-			// const waterResponse = await fetch('/api/waterData');
-		    const waterBody2 = await waterResponse2.json();
-			this.setState({ water2: waterBody2, isLoading: true });
-			// console.log(this.state.water2["waterData"]);
-
-			const natResponse2 = await fetch('/api/naturalDisasters/'+this.address2);
-			// const natResponse = await fetch('/api/naturalDisasters');
-		    const natBody2 = await natResponse2.json();
-			this.setState({ natural2: natBody2, isLoading: true });
-			// console.log(this.state.natural2["natData"]);
-			
-			const allergenResponse2 = await fetch('/api/allergenData/' + this.address2);
-			const allergenBody2 = await allergenResponse2.json();
-			this.setState({ allergen2: allergenBody2, isLoading:false});
-			// console.log(this.state.allergen2);
-
-			// const bar1Response = await fetch('/api/barChart1/'+this.address);
-			// const bar1Body = await bar1Response.json();
-			// this.setState({ bar1: bar1Body,isLoading: true });
-			// console.log(this.state.bar1["bar1Data"]);
-
-			// const allergenResponse = await fetch('/api/allergenData/' + this.address);
-			// const allergenBody = await allergenResponse.json();
-			// this.setState({ allergen: allergenBody, isLoading:true});
-			// console.log(this.state.allergen);
-
-			// const allergenResponse2 = await fetch('/api/allergenData/' + this.address2);
-			// const allergenBody2 = await allergenResponse2.json();
-			// this.setState({ allergen2: allergenBody2, isLoading:true});
-			// console.log(this.state.allergen2);
-
-		    // const bar2Response = await fetch('/api/barChart2/'+this.address);
-			// const bar2Body = await bar2Response.json();
-			// this.setState({ bar2: bar2Body,isLoading: true });
-			// console.log(this.state.bar2["bar2Data"]);
-
-			// const bar3Response = await fetch('/api/barChart3/'+this.address);
-			// const bar3Body = await bar3Response.json();
-			// this.setState({ bar3: bar3Body,isLoading: true });
-			// console.log(this.state.bar3["bar3Data"]);
-
-			// const bar1_Response = await fetch('/api/barChart1/'+this.address2);
-			// const bar1_Body = await bar1_Response.json();
-			// this.setState({ bar1_: bar1_Body,isLoading: true });
-			// console.log(this.state.bar1_["bar1Data"]);
-
-		    // const bar2_Response = await fetch('/api/barChart2/'+this.address2);
-			// const bar2_Body = await bar2_Response.json();
-			// this.setState({ bar2_: bar2_Body,isLoading: true });
-			// console.log(this.state.bar2_["bar2Data"]);
-
-			// const bar3_Response = await fetch('/api/barChart3/'+this.address2);
-			// const bar3_Body = await bar3_Response.json();
-			// this.setState({ bar3_: bar3_Body,isLoading: false });
-			// console.log(this.state.bar3_["bar3Data"]);
-			
+			const masterResponse2 = await fetch('/api/getData/'+this.address2)
+			const masterResponseBody2 = await masterResponse2.json();
+			this.setState({masterData2: masterResponseBody2,isLoading: false });
 	}
 	
 	
@@ -219,7 +114,7 @@ class Compare2 extends Component{
 									<tr>
 										<td valign="top" style ={subheaderStyle}>     
 											<MyBarChart 
-											data={this.state.air["airData"]}
+											data={this.state.masterData[0]["airData"]}
 											textAlign = 'top' 
 											/>
 											<br></br><br></br>
@@ -237,7 +132,7 @@ class Compare2 extends Component{
 								<tr>
 									<td valign="top">
 										<MyBarChart 
-										data={this.state.air2["airData"]}
+										data={this.state.masterData2[0]["airData"]}
 										textAlign = 'top' 
 										/>
 										<br></br><br></br>
@@ -260,15 +155,15 @@ class Compare2 extends Component{
 											<br></br><br></br><br></br><br></br>
 											<header>Tree Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen["Tree Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData[3]["Tree Pollen"]}</p>
 											<br></br><br></br>
 											<header>Grass Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen["Grass Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData[3]["Grass Pollen"]}</p>
 											<br></br><br></br>
 											<header>Ragweed Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen["Ragweed Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData[3]["Ragweed Pollen"]}</p>
 											<br></br><br></br>
 										</td>
 								</tr>
@@ -283,15 +178,15 @@ class Compare2 extends Component{
 										<br></br><br></br><br></br><br></br>
 											<header>Tree Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen2["Tree Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData2[3]["Tree Pollen"]}</p>
 											<br></br><br></br>
 											<header>Grass Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen2["Grass Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData2[3]["Grass Pollen"]}</p>
 											<br></br><br></br>
 											<header>Ragweed Pollen: </header>
 											<br></br>
-											<p style= {allergenInfoStyle}>{this.state.allergen2["Ragweed Pollen"]}</p>
+											<p style= {allergenInfoStyle}>{this.state.masterData2[3]["Ragweed Pollen"]}</p>
 											<br></br><br></br>
 									</td>
 								</tr>
@@ -310,7 +205,7 @@ class Compare2 extends Component{
 									<tr>
 										<td valign="top"> 
 											<MyBarChart2 
-											data={this.state.water["waterData"]}
+											data={this.state.masterData[1]["waterData"]}
 											align = 'top' />
 											<br></br><br></br>
 										</td>
@@ -324,7 +219,7 @@ class Compare2 extends Component{
 								<tr>
 									<td valign="top"> 
 										<MyBarChart2 
-										data={this.state.water2["waterData"]}
+										data={this.state.masterData2[1]["waterData"]}
 										align = 'top' />
 										<br></br><br></br>
 									</td>
@@ -343,7 +238,7 @@ class Compare2 extends Component{
 								<tbody>
 											<tr>
 												<td valign="top">
-													<MyBarChart3 data={this.state.natural["natData"]}/>
+													<MyBarChart3 data={this.state.masterData[2]["natData"]}/>
 													<br></br><br></br>
 												</td>
 												
@@ -358,7 +253,7 @@ class Compare2 extends Component{
 									<tr>
 									
 										<td valign="top">
-												<MyBarChart3 data={this.state.natural2["natData"]}/>
+												<MyBarChart3 data={this.state.masterData2[2]["natData"]}/>
 												<br></br><br></br>
 										</td>
 										
